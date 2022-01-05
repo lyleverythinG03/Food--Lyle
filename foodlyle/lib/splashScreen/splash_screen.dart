8b836d0454faa39084999cb4ba3008e44b80e63d
Lyle <lyleverythin@gmail.com>
2022-01-05 08:01:03 +0800

    splash screen auth screen initial

---
+++ foodlyle/lib/splashScreen/splash_screen.dart
@@ -0,0 +1,66 @@
+// ignore_for_file: file_names
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:foodlyle/authentication/auth_screen.dart';
+
+class MySplashScreen extends StatefulWidget {
+  const MySplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _MySplashScreenState createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+  startTimer() {
+    Timer(const Duration(seconds: 8), () async {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (c) => const AuthScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        color: Colors.white,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("images/splash.jpg"),
+              const SizedBox(
+                height: 10,
+              ),
+              const Padding(
+                padding: EdgeInsets.all(18.0),
+                child: Text(
+                  'Sell Food Online',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 40,
+                    fontFamily: "Signatra",
+                    letterSpacing: 3,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
