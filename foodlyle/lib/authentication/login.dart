585b5914c390c5ad0cd21de51e825979cc132cd2
Lyle <lyleverythin@gmail.com>
2022-01-10 13:51:09 +0800

    made initial functions for starting authentication

--- foodlyle/lib/authentication/login.dart
+++ foodlyle/lib/authentication/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foodlyle/widgets/custom_text_field.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -8,8 +9,60 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            height: 270,
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Image.asset("images/seller.png"),
+            ),
+          ),
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                CustomTextField(
+                  data: Icons.email,
+                  controller: emailController,
+                  hintText: "Email",
+                  isObscure: true,
+                ),
+                CustomTextField(
+                  data: Icons.lock,
+                  controller: passwordController,
+                  hintText: "Password",
+                  isObscure: false,
+                ),
+              ],
+            ),
+          ),
+          ElevatedButton(
+            child: const Text(
+              'Login',
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.blue,
+              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+            ),
+            onPressed: () {
+              print("clicked");
+            },
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+        ],
+      ),
+    );
   }
 }
--- foodlyle/lib/splashScreen/splash_screen.dart
+++ foodlyle/lib/splashScreen/splash_screen.dart
@@ -14,7 +14,7 @@ class MySplashScreen extends StatefulWidget {
 
 class _MySplashScreenState extends State<MySplashScreen> {
   startTimer() {
-    Timer(const Duration(seconds: 8), () async {
+    Timer(const Duration(seconds: 2), () async {
       Navigator.push(
         context,
         MaterialPageRoute(
