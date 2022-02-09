b0c0b06f276d9ceb050434c2e03c94c29ddc222c
Lyle <97102292+lyleverythinG03@users.noreply.github.com>
2022-02-09 11:21:24 +0800

    added menu collection side sellers collection
    can now upload menus, added menu model

--- foodlyle/lib/upload_screen/menus_upload.dart
+++ foodlyle/lib/upload_screen/menus_upload.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:foodlyle/global/global.dart';
 import 'package:foodlyle/mainScreens/home_screen.dart';
+import 'package:foodlyle/widgets/error_dialog.dart';
+import 'package:foodlyle/widgets/progress_bar.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart' as storageRef;
 
 class MenusUploadScreen extends StatefulWidget {
   const MenusUploadScreen({Key? key}) : super(key: key);
@@ -12,6 +18,10 @@ class MenusUploadScreen extends StatefulWidget {
 class _MenusUploadScreenState extends State<MenusUploadScreen> {
   XFile? imageXFile;
   final ImagePicker _picker = ImagePicker();
+  TextEditingController shortInfoController = TextEditingController();
+  TextEditingController titleController = TextEditingController();
+  bool uploading = false;
+  String uniqueIdName = DateTime.now().millisecondsSinceEpoch.toString();
 
   defaultScreen() {
     return Scaffold(
@@ -180,8 +190,226 @@ class _MenusUploadScreenState extends State<MenusUploadScreen> {
     });
   }
 
+  menuUploadFormScreen() {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            clearMenuUploadForm();
+          },
+        ),
+        title: const Text(
+          'Uploading',
+          style: TextStyle(
+            fontSize: 20,
+            fontFamily: "Lobster",
+          ),
+        ),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.cyan,
+                Colors.amber,
+              ],
+              begin: FractionalOffset(
+                0.0,
+                0.0,
+              ),
+              end: FractionalOffset(
+                1.0,
+                0.0,
+              ),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp,
+            ),
+          ),
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: true,
+        actions: [
+          TextButton(
+            onPressed: () {
+              uploading == true ? null : validateUploadForm();
+              setState(() {
+                uploading = true;
+              });
+            },
+            child: const Text(
+              'Add',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+                fontFamily: "Lobster",
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          uploading == true ? linearProgress() : const Text(''),
+          Container(
+            height: 230,
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Center(
+              child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: FileImage(
+                        File(imageXFile!.path),
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const Divider(
+            color: Colors.black,
+            thickness: 1,
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.perm_device_information,
+              color: Colors.cyan,
+            ),
+            title: Container(
+              width: 250,
+              child: TextField(
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+                controller: shortInfoController,
+                decoration: const InputDecoration(
+                  hintText: 'Menu Info',
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          const Divider(
+            color: Colors.black,
+            thickness: 1,
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.title,
+              color: Colors.cyan,
+            ),
+            title: Container(
+              width: 250,
+              child: TextField(
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+                controller: titleController,
+                decoration: const InputDecoration(
+                  hintText: 'Menu Title',
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          const Divider(
+            color: Colors.black,
+            thickness: 1,
+          ),
+        ],
+      ),
+    );
+  }
+
+  clearMenuUploadForm() {
+    setState(() {
+      shortInfoController.clear();
+      titleController.clear();
+      imageXFile = null;
+    });
+  }
+
+  validateUploadForm() async {
+    if (imageXFile != null) {
+      if (shortInfoController.text.isNotEmpty &&
+          titleController.text.isNotEmpty) {
+        //uploading Image
+        String downloadUrl = await uploadImage(
+          File(imageXFile!.path),
+        );
+        //save info to firebase
+        saveInfo(
+          downloadUrl,
+        );
+      } else {
+        showDialog(
+          context: context,
+          builder: (c) {
+            return ErrorDialog(
+              message: "Please write title and info for Menu",
+            );
+          },
+        );
+      }
+    } else {
+      showDialog(
+        context: context,
+        builder: (c) {
+          return ErrorDialog(
+            message: "Please pick an Image for Menu",
+          );
+        },
+      );
+    }
+  }
+
+  uploadImage(mImageFile) async {
+    storageRef.Reference reference =
+        storageRef.FirebaseStorage.instance.ref().child("menus");
+    storageRef.UploadTask uploadTask =
+        reference.child(uniqueIdName + "jpg").putFile(mImageFile);
+    storageRef.TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() {});
+    String downloadURL = await taskSnapshot.ref.getDownloadURL();
+    return downloadURL;
+  }
+
+  saveInfo(String downloadUrl) {
+    final ref = FirebaseFirestore.instance
+        .collection("sellers")
+        .doc(sharedPreferences!.getString("uid"))
+        .collection("menus");
+
+    ref.doc(uniqueIdName).set({
+      "menuID": uniqueIdName,
+      "sellerUID": sharedPreferences!.getString("uid"),
+      "menuInfo": shortInfoController.text.toString(),
+      "menuTitle": titleController.text.toString(),
+      "publishedDate": DateTime.now(),
+      "status": "available",
+      "thumbnailUrl": downloadUrl,
+    });
+    clearMenuUploadForm();
+    setState(() {
+      uniqueIdName = DateTime.now().millisecondsSinceEpoch.toString();
+      uploading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return defaultScreen();
+    return imageXFile == null ? defaultScreen() : menuUploadFormScreen();
   }
 }
---
+++ foodlyle/lib/widgets/info_design_widget.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:foodlyle/model/menus.dart';
+
+class InformationDesignWidget extends StatefulWidget {
+  final Menus? model;
+  final BuildContext? context;
+  const InformationDesignWidget({Key? key, this.model, this.context})
+      : super(key: key);
+
+  @override
+  _InformationDesignWidgetState createState() =>
+      _InformationDesignWidgetState();
+}
+
+class _InformationDesignWidgetState extends State<InformationDesignWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: Colors.amber,
+      child: Padding(
+        padding: const EdgeInsets.all(
+          5.0,
+        ),
+        child: Container(
+          height: MediaQuery.of(context).size.height * .5,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              Divider(
+                height: 4,
+                thickness: 3,
+                color: Colors.grey[300],
+              ),
+              Image.network(
+                widget.model!.thumbnailUrl!,
+                height: 210,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 1.0,
+              ),
+              Text(
+                widget.model!.menuTitle!,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 40,
+                  fontFamily: "Signatra",
+                ),
+              ),
+              Text(
+                widget.model!.menuInfo!,
+                style: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 30,
+                  fontFamily: "Signatra",
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
